b961417a51e4a20027d9eb94b6e51c647fec9bff
vriznet <vriz@vriz.net>
2018-06-28 10:38:09 +0900

    appDelegate, ItemCoreData, CustomTextField task finished

--- DreamLister_ex2/Model/Item+CoreDataClass.swift
+++ DreamLister_ex2/Model/Item+CoreDataClass.swift
@@ -11,5 +11,8 @@ import Foundation
 import CoreData
 
 public class Item: NSManagedObject {
-
+    public override func awakeFromInsert() {
+        super.awakeFromInsert()
+        self.created = NSDate()
+    }
 }
