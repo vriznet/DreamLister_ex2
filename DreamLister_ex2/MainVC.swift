266639af035625fe4037f97d2f7ba9227b6f651d
vriznet <vriz@vriz.net>
2018-06-28 10:29:56 +0900

    Designing MainVC task finished

---
+++ DreamLister_ex2/MainVC.swift
@@ -0,0 +1,17 @@
+//
+//  ViewController.swift
+//  DreamLister_ex2
+//
+//  Created by vriz on 2018. 6. 27..
+//  Copyright © 2018년 vriz. All rights reserved.
+//
+
+import UIKit
+
+class MainVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+}
